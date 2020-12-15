f2b369958d007e2edc7b09a6fd36b5b64d59eb35
Node0 <hadoop@BDDST-Group2-node0.uvm.sdu.dk>
2020-12-15 14:52:40 +0100

    Better graphics, names, and methods.

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const express = require("express"),
 ;
 
 
+var totalHistTweets = []
+
 //Defaults public path
 var public = 	"public"
 var graphs = 	"public/graphs"
@@ -21,29 +23,57 @@ app.use(express.static(graphs))
 app.use(express.static(style))
 
 //Listens for connections
-app.listen(port, () =>
-  console.log("WebUI listening at http://" + ip + ":" + port)
-)
+app.listen(port, () => {
+  	console.log("WebUI listening at http://" + ip + ":" + port)
+	fetch("http://localhost:9870/webhdfs/v1/user/hadoop/twitter-java-save-txt/V01Sql.1608024278655/?op=LISTSTATUS")
+	.then(response => response.json())
+	.then(text => {
+		var file = text.FileStatuses.FileStatus
+		file.forEach(line => {
+			var f = line
+			//Identify files (parts)
+			if(f.pathSuffix.substr(0,4) == "part"){
+				console.log("Sending file with name: " + f.pathSuffix)
+
+				//Getting the specific content of the file
+				fetch("http://localhost:9870/webhdfs/v1/user/hadoop/twitter-java-save-txt/V01Sql.1608024278655/" + f.pathSuffix + "?op=OPEN")
+				.then(response => response.text())
+				.then(file => file.split("\n"))
+				.then(tweets => {
+					//For each tweet in the file of tweets, send to the client
+					tweets.forEach(tweet => {
+						try{
+							totalHistTweets.push(JSON.parse(tweet))
+						}catch(err){
+							//console.log("Caused by: " + tweet)
+							//console.log(err)
+						}
+					})
+				})
+			}
+		})
+	})
+})
 
 //Default request
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, public, "index.html"))
 })
 
-//Wordclouds page request
-app.get("/wordClouds", function (req, res) {
-  res.sendFile(path.join(__dirname, public, "word_clouds.html"))
+//Historical page request
+app.get("/historical", function (req, res) {
+  res.sendFile(path.join(__dirname, public, "historical.html"))
 })
 
-//Get from HDFS
 app.get("/histdata", function (req, res) {
-	fetch("http://localhost:9870/webhdfs/v1/user/hadoop/twitter-java-save-txt/V01Sql.1607699461165/?op=LISTSTATUS")
-		.then(response => response.text())
-		.then(text => {
-			console.log(text)
-		})
+	console.log("Server finished transmitting " + totalHistTweets.length + " files..")
+	res.send(totalHistTweets)
+})
 
-  fetch("http://localhost:9870/webhdfs/v1/user/hadoop/twitter-java-save-txt/V01Sql.1607703257298/part-00000?op=OPEN")
-	.then(response => response.text())
-	.then(text => res.json(utf8.encode(text)))
+app.get("/live", function (req, res) {
+	res.sendFile(path.join(__dirname, public, "live.html"))
+})
+
+app.get("/livedata", function (req, res) {
+	console.log("Live data request")
 })
